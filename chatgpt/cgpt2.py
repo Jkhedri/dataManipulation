f33d0ec9f2231977a2dea6acbcd44ef0e4f0b01a
samuelhoglund <samhog@kth.se>
2023-03-23 18:11:15 +0100

    Write to JSON implementations

--- chatgpt/cgpt2.py
+++ chatgpt/cgpt2.py
@@ -24,13 +24,13 @@ def main():
                           'Please return only the data in the described format and nothing more. Understood?'
 
   # initialize the answers list
-  answers = []
+  #answers = []
 
   # get the first response
 
   time0 = timeit.default_timer()
   # loop to get multiple responses
-  for i in range(ITERATIONS):
+  for i in range(2):
     starttime = timeit.default_timer()
     # get a single response from the API
     response = openai.ChatCompletion.create(
@@ -52,10 +52,12 @@ def main():
     print("- Total runtime:", round(timeit.default_timer() - time0), "seconds", "\n")
 
     # get the answer from the response
-    answer = response['choices'][0]['message']['content']
+    answers = [response['choices'][0]['message']['content']]
+    #answers = []
     
     # append the answer to the list
-    answers.append(answer)
+    #answers.append(answer)
+    write_to_json(answers, "testdata.json")
     """
     # ask the user if they want to continue
     user_input = input("Do you want another response? (y/n): ")
@@ -67,8 +69,46 @@ def main():
     
   return answers
 
+def write_to_json(list, filename):
+    remove_last_line(filename)
+    with open(filename, "a") as outfile:
+        outfile.write(",")
+        outfile.write("\n")
+        for json_object in list:
+            outfile.write(str(json_object))
+            if json_object != list[-1]:
+                outfile.write(",")
+            outfile.write("\n")
+        outfile.write("]")
+    return outfile
+
+def remove_last_line(filename):                     # Från https://stackoverflow.com/questions/1877999/delete-final-line-in-file-with-python
+    with open(filename, "r+", encoding = "utf-8") as file:
+
+        # Move the pointer (similar to a cursor in a text editor) to the end of the file
+        file.seek(0, os.SEEK_END)
+
+        # This code means the following code skips the very last character in the file -
+        # i.e. in the case the last line is null we delete the last line
+        # and the penultimate one
+        pos = file.tell() - 1
+
+        # Read each character in the file one at a time from the penultimate
+        # character going backwards, searching for a newline character
+        # If we find a new line, exit the search
+        while pos > 0 and file.read(1) != "\n":
+            pos -= 1
+            file.seek(pos, os.SEEK_SET)
+
+        # So long as we're not at the start of the file, delete all the characters ahead
+        # of this position
+        if pos > 0:
+            file.seek(pos, os.SEEK_SET)
+            file.truncate()
+
+
 # Writing to sample.json
-def write_to_json(list):
+def write_to_json_old(list):
     with open("pnatemp0p5n6.json", "w") as outfile:
         outfile.write("[")
         outfile.write("\n")
@@ -85,6 +125,7 @@ def print_answers(answers):
         print(answer)
 
 if __name__ == "__main__":
-    answers = main()
+    main()
+    #answers = main()
     #print_answers(answers)
-    write_to_json(answers)
+    #write_to_json(answers, "testdata.json")
