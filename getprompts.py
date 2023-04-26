79f3fe7b8b24200dcfd8c0eafe7fea70a8df84ca
Jkhedri <Josefkhedri@gmail.com>
2023-04-26 17:37:31 +0200

    prompizi dot json

---
+++ getprompts.py
@@ -0,0 +1,22 @@
+import json
+
+def get_dict_list():
+    with open('main_run_data_jkhedri.json', "r") as f:
+        dict_list = json.load(f)
+    return dict_list
+
+def get_prompts():
+    dict_list = get_dict_list()
+    prompts = []
+    for i in range(len(dict_list)):
+        if type(dict_list[i]) == list:
+            print(dict_list[i])
+        prompts.append(dict_list[i].get('prompt'))
+    return prompts  
+
+if __name__ == "__main__":
+    prompts = get_prompts()
+    with open('prompts.json', 'w') as f:
+        f.write('[\n')
+        for prompt in prompts:
+            f.write("{\n" + "   \"prompt\": \"" + prompt + "\"\n},\n")
